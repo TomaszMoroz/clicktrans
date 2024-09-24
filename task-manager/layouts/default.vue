47f2abe13ea1eeae31686522a255eb1c93c0417d
TomaszMoroz <tmoroz688@gmail.com>
2024-09-24 19:00:56 +0200

    layouts: default layout

---
+++ task-manager/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>
+      <h1>Task Manager</h1>
+    </header>
+    <slot />
+    <footer>
+      <p>&copy; 2024 Task Manager</p>
+    </footer>
+  </div>
+</template>
