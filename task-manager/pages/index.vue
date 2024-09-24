df1bb0530a917eaa33a795c9cef3ca737c65b2d7
TomaszMoroz <tmoroz688@gmail.com>
2024-09-24 19:01:14 +0200

    pages: index.vue

---
+++ task-manager/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <AddTaskForm />
+    <TaskList />
+  </div>
+</template>
+
+<script setup>
+import AddTaskForm from '~/components/AddTaskForm.vue';
+import TaskList from '~/components/TaskList.vue';
+</script>
